7783843c9f54b0e07c855cef31d95d48630567c9
kent <nwekep14@gmail.com>
2024-04-30 18:45:16 +0100

    - testing local

--- frontend/src/utils/fetchData.js
+++ frontend/src/utils/fetchData.js
@@ -3,12 +3,9 @@ export async function getResources() {
 		"utils",
 		process.env.NEXT_PUBLIC_BRIDGECARD_STATUS_PAGE_BACKEND_HOST
 	);
-	const res = await fetch(
-		`http://${process.env.NEXT_PUBLIC_BRIDGECARD_STATUS_PAGE_BACKEND_HOST}:${process.env.NEXT_PUBLIC_BRIDGECARD_STATUS_PAGE_BACKEND_PORT}/v1/resource/`,
-		{
-			cache: "no-store",
-		}
-	);
+	const res = await fetch(`http://localhost:8080/v1/resource/`, {
+		cache: "no-store",
+	});
 	// The return value is *not* serialized
 	// You can return Date, Map, Set, etc.
 
@@ -21,12 +18,9 @@ export async function getResources() {
 }
 
 export async function getDowntimes() {
-	const res = await fetch(
-		`http://${process.env.NEXT_PUBLIC_BRIDGECARD_STATUS_PAGE_BACKEND_HOST}:${process.env.NEXT_PUBLIC_BRIDGECARD_STATUS_PAGE_BACKEND_PORT}/v1/downtime/`,
-		{
-			cache: "no-store",
-		}
-	);
+	const res = await fetch(`http://localhost:8080/v1/downtime/`, {
+		cache: "no-store",
+	});
 	// The return value is *not* serialized
 	// You can return Date, Map, Set, etc.
 
@@ -39,12 +33,9 @@ export async function getDowntimes() {
 }
 
 export async function getAdmin() {
-	const res = await fetch(
-		`http://${process.env.NEXT_PUBLIC_BRIDGECARD_STATUS_PAGE_BACKEND_HOST}:${process.env.NEXT_PUBLIC_BRIDGECARD_STATUS_PAGE_BACKEND_PORT}/v1/admin/`,
-		{
-			cache: "no-store",
-		}
-	);
+	const res = await fetch(`http://localhost:8080/v1/admin/`, {
+		cache: "no-store",
+	});
 	// The return value is *not* serialized
 	// You can return Date, Map, Set, etc.
 
